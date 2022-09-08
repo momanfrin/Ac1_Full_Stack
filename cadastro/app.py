6517884a46e278b0762946c0a82a53765becf094
Momanfrin <monique.manfrin@gmail.com>
2022-09-08 12:17:59 -0300

    alterações

--- cadastro/app.py
+++ cadastro/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # MySQL configuracoes para futura conexao
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
-app.config['MYSQL_DATABASE_DB'] = 'teste'
+app.config['MYSQL_DATABASE_DB'] = 'ac1'
 app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
@@ -19,12 +19,12 @@ def main():
 @app.route('/gravar', methods=['POST','GET'])
 def gravar():
   nome = request.form['nome']
-  email = request.form['email']
-  senha = request.form['senha']
-  if nome and email and senha:
+  cpf = request.form['cpf']
+  endeco = request.form['endereco']
+  if nome and cpf and endereco:
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute('insert into user (name, email, senha) VALUES (%s, %s, %s)', (nome, email, senha))
+    cursor.execute('insert into ac1 (name, cpf, endereco) VALUES (%s, %s, %s)', (nome, cpf, endereco))
     conn.commit()
   return render_template('index.html')
 
@@ -33,7 +33,7 @@ def gravar():
 def listar():
   conn = mysql.connect()
   cursor = conn.cursor()
-  cursor.execute('select name, email, senha from user')
+  cursor.execute('select name, cpf, endereco from ac1')
   data = cursor.fetchall()
   conn.commit()
   return render_template('lista.html', datas=data)
